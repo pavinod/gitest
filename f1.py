88e29a864cba7e9b21e8511cb1ade3bc95881326
viren <pavithren@mymail.sutd.edu.sg>
2015-11-13 15:55:14 +0800

    added add

--- f1.py
+++ f1.py
@@ -5,6 +5,12 @@ Create 3 functions: multiply, add, divide for the calculator class
 Each student should make their own branch and work on one of the functions in 
 the corresponding file.
 123456789
+
+
+
+
+
+THANKS Peh-QinCheng !!!!!
 """
 
 import multiply as ml
@@ -16,6 +22,9 @@ class Calculator:
     
     def  __init__(self):
         return None
-        
+    def addition (self,x1,x2):
+        return ad.add(x1,x2)
+
+            
         
         
